8526f653ec0810f381d400b3e20da7107ef7fd7b
hannahbako <arri_hannah@yahoo.com>
2018-09-30 21:40:25 -0400

    Location Service Created for application

--- app/src/main/java/com/latinocodes/safeme/SafeMeActivity.java
+++ app/src/main/java/com/latinocodes/safeme/SafeMeActivity.java
@@ -1,7 +1,14 @@
 package com.latinocodes.safeme;
 
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.app.Service;
+import android.widget.Toast;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
 
 public class SafeMeActivity extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class SafeMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safe_me);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+//            googleMap.setMyLocationEnabled(true);
+//            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            Toast.makeText(this,"Location Enabled", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    1);
+        }
+        startService(new Intent(this,LocationService .class));
+
     }
 }
