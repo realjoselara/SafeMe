e8a8130b80b6760d2794e833262b7132cd3c752d
latinocodes <joselara02@outlook.com>
2018-10-05 11:12:32 -0400

    fixed conflicts with activity id's nameing, breaking the app to crash

--- app/src/main/java/com/latinocodes/safeme/SafeMeActivity.java
+++ app/src/main/java/com/latinocodes/safeme/SafeMeActivity.java
@@ -4,8 +4,6 @@ import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.app.Service;
 import android.widget.Toast;
 import android.support.v4.app.ActivityCompat;
 import android.Manifest;
