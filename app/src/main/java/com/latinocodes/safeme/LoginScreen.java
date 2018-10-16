996160c219003b40ddeab9275fc3c6a6cabc9007
michaelkim57 <michaelkim57@gmail.com>
2018-10-16 19:46:12 -0400

    -Login Button is now connected to FireBase Auth -Validates the email address and password through Firebase Auth -Toast returned if login is successful -Toast returned if email or password does not meet certain requirements -Added Volumedown keypress event to send out alert once it is set up

--- app/src/main/java/com/latinocodes/safeme/LoginScreen.java
+++ app/src/main/java/com/latinocodes/safeme/LoginScreen.java
@@ -1,26 +1,46 @@
 package com.latinocodes.safeme;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.util.Patterns;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
     private Button button;
     private Button signButton;
+
+    //Firebase Generated Code
+    FirebaseAuth mAuth;
+
+    EditText editemailaddress, editpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        mAuth = FirebaseAuth.getInstance();
+        editemailaddress = findViewById(R.id.loginEmailAddress);
+        editpassword = findViewById(R.id.loginpassword);
+
+
         button = findViewById(R.id.loginButton);
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSafeMeActivity();
+                logintosafeme();
+
             }
         });
 
@@ -29,9 +49,62 @@ public class LoginScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openSignUpActivity();
+
             }
         });
+    }
+    private void logintosafeme(){
+        try {
+            //get email and password from the two text fields
+            //declare variable from the text fields
+            String useremail = editemailaddress.getText().toString().trim();
+            String userpass = editpassword.getText().toString().trim();
 
+            //validate text fields: Email address and Password
+            //Validate the fields
+            if (useremail.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Email Required", Toast.LENGTH_SHORT).show();
+                editemailaddress.requestFocus();
+                return;
+            }
+
+            //Regex Email address pattern
+            if (!Patterns.EMAIL_ADDRESS.matcher(useremail).matches()) {
+                Toast.makeText(getApplicationContext(), "Enter Valid Email Address", Toast.LENGTH_SHORT).show();
+                editemailaddress.requestFocus();
+                return;
+            }
+
+            //validate password is empty
+            if (userpass.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Password Required", Toast.LENGTH_SHORT).show();
+                editpassword.requestFocus();
+                return;
+            }
+            //validate password length
+            if (userpass.length() < 6) {
+                Toast.makeText(getApplicationContext(), "Password should be at least 6 characters", Toast.LENGTH_SHORT).show();
+                editpassword.requestFocus();
+                return;
+            }
+
+            //sign in method from FirebaseAuth
+            mAuth.signInWithEmailAndPassword(useremail, userpass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Login Complete", Toast.LENGTH_SHORT).show();
+
+                        openSafeMeActivity();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please check Email Address or Password", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }catch(Exception ex){
+            System.out.println("Error Exception has occurred");
+        }
     }
     public void openSafeMeActivity(){
         Intent intent = new Intent(this, SafeMeActivity.class);
--- app/src/main/java/com/latinocodes/safeme/SafeMeActivity.java
+++ app/src/main/java/com/latinocodes/safeme/SafeMeActivity.java
@@ -11,12 +11,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import android.support.v4.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
+import android.widget.EditText;
 
 import java.util.HashMap;
 
@@ -25,6 +27,10 @@ public class SafeMeActivity extends AppCompatActivity {
     public HashMap<String, Double> coordinates = new HashMap<>();
     public static final String BROADCAST_ACTION = "com.ojastec.broadcastreceiverdemo";
     private String TAG = "SafeMeActivity";
+
+    EditText editemailaddress, editpassword;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +42,16 @@ public class SafeMeActivity extends AppCompatActivity {
                         PackageManager.PERMISSION_GRANTED) {
 //            googleMap.setMyLocationEnabled(true);
 //            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-            Toast.makeText(this,"Location Enabled", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Location Enabled", Toast.LENGTH_LONG).show();
         } else {
-            ActivityCompat.requestPermissions(this, new String[] {
+            ActivityCompat.requestPermissions(this, new String[]{
                             Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
                     1);
         }
 
 //        startService(new Intent(LOCATION_SERVICE));
-        Intent locationService = new Intent(this,LocationService .class);
+        Intent locationService = new Intent(this, LocationService.class);
         startService(locationService);
 
         registerMyReceiver();
@@ -103,82 +109,98 @@ public class SafeMeActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
 
-    //openPopup Method after pushing one of the buttons on SafeMeActivity Screen
-    public void openPopup() {
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-        alertBuilder.setTitle("Alert Sent")
-                .setMessage("An Alert has been sent Out!")
-                .setPositiveButton("Find Help", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                            String uri = "geo"+coordinates.get("latitude")+", "+ coordinates.get("longitude");
-                            String gweburi = "http://maps.google.com/maps?saddr="+coordinates.get("latitude")+","+ coordinates.get("longitude") +"&daddr=" + 40.7449992 + "," + -74.0239707;
+
+        //openPopup Method after pushing one of the buttons on SafeMeActivity Screen
+        public void openPopup () {
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.setTitle("Alert Sent")
+                    .setMessage("An Alert has been sent Out!")
+                    .setPositiveButton("Find Help", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String uri = "geo" + coordinates.get("latitude") + ", " + coordinates.get("longitude");
+                            String gweburi = "http://maps.google.com/maps?saddr=" + coordinates.get("latitude") + "," + coordinates.get("longitude") + "&daddr=" + 40.7449992 + "," + -74.0239707;
 //                            String gweburi = "http://maps.google.com/maps?&daddr=" + 40.7449992 + "," + -74.0239707;
                             Uri gmapsIntent = Uri.parse(uri);
                             Uri gwebIntent = Uri.parse(gweburi);
                             Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmapsIntent);
                             mapIntent.setPackage("com.google.android.apps.maps");
-                        if (mapIntent.resolveActivity(getPackageManager()) != null) {
-                            System.out.println("ARRR!!&*)*()(_"+uri);
-                            startActivity(mapIntent);
-                        }else{
+                            if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                                System.out.println("ARRR!!&*)*()(_" + uri);
+                                startActivity(mapIntent);
+                            } else {
 
-                            mapIntent= new Intent(Intent.ACTION_VIEW, gwebIntent);
-                            System.out.println("ARRR!!&*)*()(_"+gweburi);
-                            startActivity(mapIntent);
+                                mapIntent = new Intent(Intent.ACTION_VIEW, gwebIntent);
+                                System.out.println("ARRR!!&*)*()(_" + gweburi);
+                                startActivity(mapIntent);
+                            }
+
                         }
+                    })
+                    .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                })
-                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .show();
-    }
-
-
-    /**
-     * This method is responsible to register an action to BroadCastReceiver
-     * */
-    private void registerMyReceiver() {
-
-        try
-        {
-            IntentFilter intentFilter = new IntentFilter();
-            intentFilter.addAction(BROADCAST_ACTION);
-            this.registerReceiver(new MyBroadcastReceiver(), intentFilter);
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
+                        }
+                    })
+                    .show();
         }
 
-    }
 
-    /**This class extends BroadcastReceiver and defines the onReceieve method**/
-    class MyBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context ctxt, Intent i) {
-            /**This method overrides the BroadcastReciever.onReceieve method. When called
-             * it pulls the users location coordinates passed as Intent extras from the service**/
+        /**
+         * This method is responsible to register an action to BroadCastReceiver
+         * */
+        private void registerMyReceiver () {
+
             try {
-                Log.d(TAG, "onReceive() called");
-
-                // uncomment this line if you had sent some data
-
-                coordinates.put("longitude", i.getDoubleExtra("long", 0));
-                coordinates.put("latitude", i.getDoubleExtra("lat", 0));
-                Log.e(TAG, "coordinates received");
-
+                IntentFilter intentFilter = new IntentFilter();
+                intentFilter.addAction(BROADCAST_ACTION);
+                this.registerReceiver(new MyBroadcastReceiver(), intentFilter);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
+
         }
+
+        /**This class extends BroadcastReceiver and defines the onReceieve method**/
+        class MyBroadcastReceiver extends BroadcastReceiver {
+            @Override
+            public void onReceive(Context ctxt, Intent i) {
+                /**This method overrides the BroadcastReciever.onReceieve method. When called
+                 * it pulls the users location coordinates passed as Intent extras from the service**/
+                try {
+                    Log.d(TAG, "onReceive() called");
+
+                    // uncomment this line if you had sent some data
+
+                    coordinates.put("longitude", i.getDoubleExtra("long", 0));
+                    coordinates.put("latitude", i.getDoubleExtra("lat", 0));
+                    Log.e(TAG, "coordinates received");
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Toast.makeText(getApplicationContext(), "Signed Out", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+            Toast.makeText(getApplicationContext(), "Emergency Alert Sent!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
+
+
