5c2f9bdffd51fdd5167b38322a0883eb3ab13071
michaelkim57 <michaelkim57@gmail.com>
2018-10-29 21:55:37 -0400

    RegisterActivity Added after Email Reg Activity - Need to Fix UI
    Sends User Info from RegisterActivityScreen to Firebase DB

---
+++ app/src/main/java/com/latinocodes/safeme/RegisterActivity.java
@@ -0,0 +1,128 @@
+package com.latinocodes.safeme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.latinocodes.safeme.model.User;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Edit textbox
+    EditText regFirstName, regLastName, regEthnicity, regGender, regAge;
+
+    String MyPREFERENCES = "UserSession";
+    SharedPreferences sharedpreferences;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        database = FirebaseDatabase.getInstance();
+
+        //set variable for email and password
+        regFirstName = findViewById(R.id.regFirstName);
+        regLastName = findViewById(R.id.regLastName);
+        regEthnicity = findViewById(R.id.regEthnicity);
+        regGender = findViewById(R.id.regGender);
+        regAge = findViewById(R.id.regAge);
+
+        findViewById(R.id.regActButton).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.regActButton:
+                //on Register Button - Send User Info to Database
+                regUserInfo();
+
+                break;
+        }
+    }
+
+    private void regUserInfo() {
+        //declare variable from the text fields
+        String firstname = regFirstName.getText().toString().trim();
+        String lastname = regLastName.getText().toString().trim();
+        String ethnicity = regEthnicity.getText().toString().trim();
+        String gender = regGender.getText().toString().trim();
+        String age = regAge.getText().toString().trim();
+
+        try {
+
+            //Validate the fields
+            if (firstname.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "First Name Required", Toast.LENGTH_SHORT).show();
+                regFirstName.requestFocus();
+                return;
+            }
+
+            if (lastname.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Last Name Required", Toast.LENGTH_SHORT).show();
+                regLastName.requestFocus();
+                return;
+            }
+
+            if (ethnicity.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Ethnicity Required", Toast.LENGTH_SHORT).show();
+                regEthnicity.requestFocus();
+                return;
+            }
+
+            if (gender.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Gender Required. Male or Female?", Toast.LENGTH_SHORT).show();
+                regGender.requestFocus();
+                return;
+            }
+
+            if (age.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Age Required", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Gson gson = new Gson();
+            String json = sharedpreferences.getString("Userinfo", "");
+            User user = gson.fromJson(json, User.class);
+
+            String uuid = user.getUserID();
+
+            createuser(uuid, firstname, lastname, ethnicity, gender, age);
+
+            Intent loginScreen = new Intent(this, LoginScreen.class);
+            startActivity(loginScreen);
+
+            Toast.makeText(getApplicationContext(),"Registered", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception ex) {
+            System.out.println("Error Exception has occurred");
+        }
+
+    }
+
+    public void createuser(String uuid, String firstname, String lastname, String ethnicity, String gender, String age){
+        /***Add user information to the database***/
+        //TODO create activity and plug this in
+        DatabaseReference user = database.getReference("Users");
+        user.child(uuid).child("UUID").setValue(uuid);
+        user.child(uuid).child("Firstname").setValue(firstname);
+        user.child(uuid).child("Lastname").setValue(lastname);
+        user.child(uuid).child("Ethnicity").setValue(ethnicity);
+        user.child(uuid).child("Sex").setValue(gender);
+        user.child(uuid).child("Age").setValue(age);
+    }
+
+
+}
