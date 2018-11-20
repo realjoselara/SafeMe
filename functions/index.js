df0780b365391fdddc9364d54ab9a0a3ce0034d7
latinocodes <joselara02@outlook.com>
2018-11-20 17:29:52 -0500

    connection function to database event successfully

--- functions/index.js
+++ functions/index.js
@@ -3,45 +3,15 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp({
+    databaseURL: 'https://ssw695-3ec44.firebaseio.com/'
 
-exports.sendNotification = functions.database.ref('/Notifications/')
-    .onWrite(event => {
-        const message = event.data.current.val();
-        const senderUid = message.from;
-        const receiverUid = message.to;
-        const promises = [];
+});
 
-        if (senderUid == receiverUid) {
-            //if sender is receiver, don't send notification
-            promises.push(event.data.current.ref.remove());
-            return Promise.all(promises);
-        }
-
-        console.log('something happened');
-
-        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
-        const getReceiverUidPromise = admin.auth().getUser(receiverUid);
-
-        return Promise.all([getInstanceIdPromise, getReceiverUidPromise]).then(results => {
-            const instanceId = results[0].val();
-            const receiver = results[1];
-            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
-
-            const payload = {
-                notification: {
-                    title: receiver.displayName,
-                    body: message.body,
-                    icon: receiver.photoURL
-                }
-            };
-
-            admin.messaging().sendToDevice(instanceId, payload)
-                .then(function (response) {
-                    console.log("Successfully sent message:", response);
-                })
-                .catch(function (error) {
-                    console.log("Error sending message:", error);
-                });
-        });
+// Function calls
+exports.sendNotificationAlert = functions.database.ref('/Notifications/{pushId}')
+    .onWrite(event  => {
+        console.log(event.after);
+        return event.after;
     });
+
