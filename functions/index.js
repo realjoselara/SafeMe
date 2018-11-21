ecd5a3cf215cd64a847f455b7adaeb46f6604d04
latinocodes <joselara02@outlook.com>
2018-11-20 21:44:02 -0500

    changin functiong

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,21 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp({
-    databaseURL: 'https://ssw695-3ec44.firebaseio.com/'
-
-});
+admin.initializeApp(functions.config().firebase);
 
 // Function calls
-exports.sendNotificationAlert = functions.database.ref('/Notifications/{pushId}')
-    .onWrite(event  => {
-        console.log(event.before._data);
-        return 0;
+exports.sendNotificationAlert = functions.database
+    .ref('/Notifications/{pushId}')
+    .onWrite((snapshot, context)  => {
+
+        var location = context.params.Location;
+        var type = context.params.Type;
+        var description = context.params.Description;
+        var date = context.params.Date;
+        console.log(`New message ${type}, location ${location}, description: ${description}, date: ${date}`)
+
+        const messageData = snapshot.val();
+        console.log(messageData)
+        return messageData;
+
     });
